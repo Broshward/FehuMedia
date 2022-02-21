1344ea277707bf14ebb868ef181ccfbb831bf882
Broshward <HarmoniesWorlds@gmail.com>
2022-02-22 01:17:33 +0300

    python translating

---
+++ add_comment.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python2
+
+# $1 - file name
+# $2 - comment string
+
+import sys,os
+add_comment=''.join(sys.argv[2:])
+filename=sys.argv[1]
+
+comment=os.popen("get_comment %s" % (filename)).read()
+if "User Comment" in comment: # For exist comments
+    comment = comment.split(':',1)[1].strip()
+if comment!='':
+    comment=comment+';'+add_comment
+else:
+    comment=add_comment
+
+img_time=os.path.getmtime(filename)
+
+#exiv2 -M'set Exif.Photo.UserComment '$comment_str $1
+#exiv2 -c $comment_str $1
+#ffmpeg -i $1 -c copy -metadata Comment=$comment_str out_$1
+cmd='/usr/bin/vendor_perl/exiftool %s -UserComment=\'%s\'' %(filename,comment)
+os.system(cmd)
+#/usr/bin/vendor_perl/exiftool -Comment=$comment_str $1
+
+os.utime(filename, (img_time,img_time))
+
+

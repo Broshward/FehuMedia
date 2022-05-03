f22d2bb716994b5b466760cc18058f5297b3cac8
Broshward <HarmoniesWorlds@gmail.com>
2022-05-03 23:19:05 +0300

    Splitting video for_twi.py to 2 min 20 sec

--- for_twi.py
+++ for_twi.py
@@ -10,12 +10,17 @@ if not os.path.exists(target_dir):
     os.mkdir(target_dir)
 
 for i in sys.argv[1:]:
+    i=os.path.abspath(i)
     if i.lower().endswith('jpg') or i.lower().endswith('png'): 
         os.system("resize1024 %s %s" %(i,target_dir+'/'+i.rsplit('/',1)[1]))
-    if i.lower().endswith('mpg') or \
+    elif i.lower().endswith('mpg') or \
        i.lower().endswith('mp4') or \
        i.lower().endswith('mov'):
-        #os.system("ffmpeg -i %s -vcodec libx264 -acodec aac -pix_fmt yuv420p -y %s.mp4" %(i,target_dir+'/'+i.rsplit('/',1)[1]))
-        os.system("ffmpeg -i %s -vcodec libx264 -acodec aac -pix_fmt yuv420p -y -map 0 -segment_time 130 -f segment %s_%%02d.mp4" %(i,target_dir+'/'+i.rsplit('/',1)[1]))
+       time = float(os.popen('ffprobe -v error -show_entries format=duration -of default=noprint_wrappers=1:nokey=1 %s' %(i)).read())
+       if time > 140:
+           print i
+           os.system("ffmpeg -i %s -vcodec libx264 -acodec aac -pix_fmt yuv420p -y -map 0 -segment_time 130 -f segment -reset_timestamps 1 %s_%%02d.mp4" %(i,target_dir+'/'+i.rsplit('/',1)[1]))
+       else: 
+            os.system("ffmpeg -i %s -vcodec libx264 -acodec aac -pix_fmt yuv420p -y %s.mp4" %(i,target_dir+'/'+i.rsplit('/',1)[1]))
         
     

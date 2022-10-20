6806a194e38f29d0827863ae4a27794b99d90ea2
Broshward <HarmoniesWorlds@gmail.com>
2022-10-20 03:51:30 +0300

    1. Add times and tempos file template creating for each input file\n2. The comment (after "#") bug fixed.\n3.Frame rate bug fixed.\n4.Disable ounput frame rate option

--- video_tempo.py
+++ video_tempo.py
@@ -35,12 +35,24 @@ Options:
     -h                          Print this help
     --tempos-file filename      times_rates_file name. Default is "times_rates_file" in current dir.
     -t num                      Change tempo of all video file to num. The num could be float.
-    -r num                      num is framerate output video. Default is 30. The num could be float.
+##    -r num                      num is framerate output video. Default is 30. The num could be float.
     -o filename                 Output video filename. Default is out.[input_ext]. 
                                 If extentions of input and output files coincides then 
                                 it will be copy video content for ScaleFactors equal "1".
                                 Else it will be convert to other format video.
 """
+
+tempos_template = '''#Format string: FirstTime-LastTime,Tempo
+# if only LastTime given, the FirstTime take from LastTime of previous line
+# end - the end of video
+# 'lost' is constant for frequently usage tempos
+# 'copy' is equvalent to 1, but it without video reconversion (for faster perfomance)
+0-1,1
+1-2,1
+3,copy
+end,lost
+lost,copy
+'''
 
 import sys,os
 if '-h' in sys.argv:
@@ -49,27 +61,23 @@ if '-h' in sys.argv:
 
 if '-r' in sys.argv:
     r_index=sys.argv.index('-r')
-    output_rate = int(sys.argv[r_index+1]) 
+    output_rate = '-filter:v fps=fps='+sys.argv[r_index+1]
     sys.argv.pop(r_index)
     sys.argv.pop(r_index)
 else:
-    output_rate = 30
+    output_rate = ''
   
 if '--times-tempos' in sys.argv:
     r_index=sys.argv.index('--times-tempos')
     times_rates_file = sys.argv[r_index+1]
     sys.argv.pop(r_index)
     sys.argv.pop(r_index)
-else:
-    times_rates_file = "./times_rates_file"
   
 if '-o' in sys.argv:
     r_index=sys.argv.index('-o')
     output_video = sys.argv[r_index+1]
     sys.argv.pop(r_index)
     sys.argv.pop(r_index)
-else:
-    output_video = "output"
 
 if '-t' in sys.argv:
     r_index=sys.argv.index('-t')
@@ -90,9 +98,14 @@ if not os.path.exists(input_video):
     print "Input video file not found!"
     exit(-2)
 
+input_rate = float(os.popen("ffmpeg -i %s 2>&1 |grep fps" %(input_video)).read().split('fps',1)[0].rsplit(',',1)[1].strip())
+print input_rate
+
 outvideo_time=os.path.getmtime(input_video)
 
 # Output video filename calculate
+if 'output_video' not in globals():
+    output_video = input_video.rsplit('.',1)[0]+'_tempo' #Default output filename
 if '.' not in output_video:
     output_video += '.'+input_video.rsplit('.',1)[1]
 
@@ -108,14 +121,22 @@ if 'all_tempo' in globals():
     os.system('ffmpeg -r %f -i %s -an -r %f %s -y %s' %(all_tempo*output_rate, input_video, output_rate, mpg_bitrate, output_video))
     exit(0)
 
-# If '-t' not given
+if 'times_rates_file' not in globals():
+    times_rates_file = input_video.rsplit('.',1)[0]+'_tempos' #Default times and tempos filename
+
+# If times_rates_file not exists we need create this. 
 if not os.path.exists(times_rates_file):
-    print "The %s file not found!" %(times_rates_file)
-    exit(-2)
+    f=open(times_rates_file,'wt')
+    f.write(tempos_template)
+    f.close()
+    print "Please edit times_tempos file %s and restart program!"
+    exit(0)
 
 times_rates=[]
 for i in open(times_rates_file).readlines():
     if i.strip() == '':
+        continue
+    if i.strip().startswith('#'):
         continue
     times,tempo = i.strip().split(',')
     if times=='lost':
@@ -124,12 +145,8 @@ for i in open(times_rates_file).readlines():
     elif '-' in times:
         BeginTime,EndTime=times.split('-')
     else:
-        if times[0]=='#':
-            BeginTime = '#'+EndTime 
-            EndTime = times[1:]
-        else:
-            BeginTime = EndTime 
-            EndTime = times
+        BeginTime = EndTime 
+        EndTime = times
     times_rates.append([BeginTime,EndTime,tempo])
 
 #spliting and rating video
@@ -137,9 +154,6 @@ list_file=open('list','w')
 name=0
 for i in times_rates:
     print i
-    if i[0][0] == '#' or i[1][0] == '#':
-        name += 1
-        continue
     if i[2]=='lost':
         i[2]=lost_tempo
     i[0]=float(i[0])
@@ -149,7 +163,7 @@ for i in times_rates:
         cmd = cmd.replace('-t','' if i[1]=='end' else '-t %f' %(float(i[1])-i[0]))
     else:
         i[2]=float(i[2])
-        cmd = 'ffmpeg -r %f -i %s -an -ss %f -t -r %f %s -y %s' %(output_rate*i[2], input_video, i[0]/i[2], output_rate, mpg_bitrate, temp_name)
+        cmd = 'ffmpeg -r %f -i %s -an -ss %f -t -r %f %s %s -y %s' %(input_rate*i[2], input_video, i[0]/i[2], input_rate, output_rate, mpg_bitrate, temp_name)
         cmd = cmd.replace('-t','' if i[1]=='end' else '-t %f' %((float(i[1])-i[0])/i[2]))
 
     list_file.write('file \'%s\'\n' %(temp_name))
@@ -160,9 +174,10 @@ list_file.close()
 #Concatenate parts from temporarily files to output 
 cmd='ffmpeg -f concat -i list -safe 0 -c copy %s' %(output_video)
 print cmd
-os.system(cmd)
+if os.system(cmd)!=0:
+    exit(-127)
 
-os.utime(outvideo, (outvideo_time,outvideo_time))
+os.utime(output_video, (outvideo_time,outvideo_time))
 
 #Remove temporarily files
 for i in open('list').readlines():

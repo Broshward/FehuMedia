bb1673a31648a48f4bf355e2a1002d44f10a230a
Broshward <HarmoniesWorlds@gmail.com>
2023-02-07 09:49:44 +0300

    --temp-dir option add

--- concat_videos.py
+++ concat_videos.py
@@ -7,6 +7,7 @@ usage='''
         --sort-time     Make sorting files as time increasing 
         --resolution    Scale output video for resolution
         -o filename     Output video filename
+        --temp-dir temp_dir         When launchs program in tmpfs, temporarily files volume may be too large and it will have make the "No space left" error. --temp-dir option will be create temporarily files in the temp_dir directory
 ''' %(sys.argv[0])
 
 if '--help' in sys.argv:
@@ -23,6 +24,14 @@ if '--resolution' in sys.argv:
     resolution = sys.argv[sys.argv.index('--resolution')+1]
     sys.argv.pop(sys.argv.index('--resolution')+1)
     sys.argv.pop(sys.argv.index('--resolution'))
+
+if '--temp-dir' in sys.argv:
+    temp_dir = sys.argv[sys.argv.index('--temp-dir')+1]
+    if not temp_dir.endswith('/'):
+        temp_dir += '/'
+    sys.argv.pop(sys.argv.index('--temp-dir')+1)
+    sys.argv.pop(sys.argv.index('--temp-dir'))
+
 
 if '-o' in sys.argv:
     outvideo = sys.argv[sys.argv.index('-o')+1]
@@ -49,24 +58,28 @@ if sort=='time':
 if 'output_video' not in globals():
     output_video = list_names[0].rsplit('.',1)[0]+'_concat.'+list_names[0].rsplit('.',1)[1] #Default output filename
 
-concat_str='concat:'
+ts_list=[]
 for i in list_names:
     if os.path.exists(i):
+        if 'temp_dir' in globals():
+            ts_name = temp_dir + i.rsplit('/',1)[1]
+        else:
+            ts_name = i 
+        ts_name += '.ts'
         if os.system('ffmpeg -i %s 2>&1|grep Audio' %(i))==0: # Have you audio stream in file?
-            cmd = 'ffmpeg -i %s -c copy %s.ts' %(i,i)   # Yes, it consists audio
+            cmd = 'ffmpeg -i %s -c copy %s' %(i,ts_name)   # Yes, it consists audio
         else:                                           # No, to need add silent audio!!
-            cmd = 'ffmpeg -f lavfi -i anullsrc=channel_layout=stereo:sample_rate=44100 -i %s -c:v copy -c:a aac -shortest  %s.ts' %(i,i)
+            cmd = 'ffmpeg -f lavfi -i anullsrc=channel_layout=stereo:sample_rate=44100 -i %s -c:v copy -c:a aac -shortest %s' %(i,ts_name)
         print cmd
         os.system(cmd)
-        concat_str += i+'.ts|'
-concat_str = concat_str[:-1]
+        ts_list.append(ts_name)
 
+concat_str='concat:'+'|'.join(ts_list)
 cmd = "ffmpeg -i '%s' -c copy -y %s" %(concat_str,output_video)
 print cmd
 os.system(cmd)
-for i in list_names:
-    cmd = 'rm %s' %(i+'.ts')
-    os.system(cmd)
+for i in ts_list:
+    os.remove(i)
 
 os.utime(output_video, (times[-1][0],times[-1][0]))
 

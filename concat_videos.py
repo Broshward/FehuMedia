c9ef0c284f44dc16fc30c15e5b24c63b4c2ba930
Broshward <HarmoniesWorlds@gmail.com>
2023-05-19 16:13:15 +0300

    --crossing-add options for add smooth crossing between concatenate videos

--- concat_videos.py
+++ concat_videos.py
@@ -8,6 +8,7 @@ usage='''
         --resolution    Scale output video for resolution
         -o filename     Output video filename
         --temp-dir temp_dir         When launchs program in tmpfs, temporarily files volume may be too large and it will have make the "No space left" error. --temp-dir option will be create temporarily files in the temp_dir directory
+        --crossing-add  Add fluent crossing between videos.
 ''' %(sys.argv[0])
 
 if '--help' in sys.argv:
@@ -38,6 +39,10 @@ if '-o' in sys.argv:
     sys.argv.pop(sys.argv.index('-o')+1)
     sys.argv.pop(sys.argv.index('-o'))
 
+if '--crossing-add' in sys.argv:
+    sys.argv.remove('--crossing-add')
+    crossing = True
+
 if len(sys.argv)==1:
     print '\nEmpty input files list !!!\n' 
     exit(-4)
@@ -49,7 +54,6 @@ for i in list_names:
     times.append([os.path.getmtime(i),i])
 times.sort()
 
-#import pdb;pdb.set_trace()
 if sort=='time':
     for i in range(len(times)):
         list_names[i]=times[i][1]
@@ -59,20 +63,44 @@ if 'output_video' not in globals():
     output_video = list_names[0].rsplit('.',1)[0]+'_concat.'+list_names[0].rsplit('.',1)[1] #Default output filename
 
 ts_list=[]
-for i in list_names:
-    if os.path.exists(i):
+for i in range(len(list_names)):
+    if 'crossing' in globals():
+        if crossing == True:
+            #import pdb;pdb.set_trace()
+            if i==0:
+                #get last image
+                os.system('ffmpeg -sseof -1 -i %s -vframes 1 -q:v 1 -qmin 1 -update 1 %s ' %(list_names[i], list_names[i]+'_last.jpg'))
+            else:
+                if i==len(list_names)-1:
+                    #get first image
+                    os.system('ffmpeg -i %s -vframes 1 -q:v 1 -qmin 1 -update 1 %s' %(list_names[i], list_names[i]+'_first.jpg'))
+                else:
+                    #get first and last images
+                    os.system('ffmpeg -i %s -sseof -1 -i %s -map 0:v -vframes 1 -q:v 1 -qmin 1 %s -map 1:v -q:v 1 -qmin 1 -update 1 %s' %(list_names[i],list_names[i], list_names[i]+'_first.jpg', list_names[i]+'_last.jpg'))
+                cross_name=list_names[i].rsplit('.',1)[0]+'_cross.mp4' # name for crossing
+                #Make pause without slides
+                os.system('image_for_video.py -o %s --slideshow --framerate 10 --duration 0,1 %s ' %(cross_name, list_names[i-1]+'_last.jpg '+list_names[i]+'_first.jpg'))
+                os.remove(list_names[i-1]+'_last.jpg')
+                os.remove(list_names[i]+'_first.jpg')
+                ts_name = cross_name+'.ts'
+                os.system('ffmpeg -i %s -c copy %s' %(cross_name,ts_name))
+                os.remove(cross_name)
+                ts_list.append(ts_name)
+
+    if os.path.exists(list_names[i]):
         if 'temp_dir' in globals():
-            ts_name = temp_dir + i.rsplit('/',1)[1]
+            ts_name = temp_dir + list_names[i].rsplit('/',1)[1]
         else:
-            ts_name = i 
+            ts_name = list_names[i] 
         ts_name += '.ts'
-        if os.system('ffmpeg -i %s 2>&1|grep Audio' %(i))==0: # Have you audio stream in file?
-            cmd = 'ffmpeg -i %s -c copy %s' %(i,ts_name)   # Yes, it consists audio
+        if os.system('ffmpeg -i %s 2>&1|grep Audio' %(list_names[i]))==0: # Have you audio stream in file?
+            cmd = 'ffmpeg -i %s -c copy %s' %(list_names[i],ts_name)   # Yes, it considers audio
         else:                                           # No, to need add silent audio!!
-            cmd = 'ffmpeg -f lavfi -i anullsrc=channel_layout=stereo:sample_rate=44100 -i %s -c:v copy -c:a aac -shortest %s' %(i,ts_name)
+            cmd = 'ffmpeg -f lavfi -i anullsrc=channel_layout=stereo:sample_rate=44100 -i %s -c:v copy -c:a aac -shortest %s' %(list_names[i],ts_name)
         print cmd
         os.system(cmd)
         ts_list.append(ts_name)
+        
 
 concat_str='concat:'+'|'.join(ts_list)
 cmd = "ffmpeg -i '%s' -c copy -y %s" %(concat_str,output_video)

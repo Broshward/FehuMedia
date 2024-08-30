84aea7271f7bd42d7ad5ff3729c9934f2b0eff9e
Broshward <HarmoniesWorlds@gmail.com>
2024-08-31 00:18:24 +0300

    Autorotate images throught exif data.

---
+++ autorotate.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python2
+#coding:utf8
+
+import sys,os,time
+
+usage='''
+%s [files]
+
+This program using for autoratating media files of cameras which saving orientation info. Witout this step it may be wrongly joining with ffmpeg utility.
+'''
+
+if len(sys.argv)==1:
+    print usage
+for i in sys.argv[1:]:
+    i_time=os.path.getmtime(i)
+    os.system('exiftran -a -i %s' %(i))
+    os.utime(i, (i_time,i_time))

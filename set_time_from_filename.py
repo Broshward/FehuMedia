206166d65322b423f1d58df292796ce311f7682c
Broshward <HarmoniesWorlds@gmail.com>
2024-08-31 00:16:59 +0300

    some bugs

--- set_time_from_filename.py
+++ set_time_from_filename.py
@@ -12,7 +12,9 @@ This program takes image or video timestamps from android similar filenames and 
 if len(sys.argv)==1:
     print usage
 for i in sys.argv[1:]:
-    date = i.split('_')[-2]
-    _time = i.split('_')[-1].split('.')[0]
-    media_time = int(time.mktime((int(date[0:4]),int(date[4:6]),int(date[6:]),int(_time[0:2]),int(_time[2:4]),int(_time[4:]),-1,-1,-1)))
+    try:date = i.rsplit('/',1)[1].split('_')[1]
+    except:import pdb;pdb.set_trace()
+    _time = i.rsplit('/',1)[1].split('_')[2].split('.')[0]
+    try:media_time = time.mktime((int(date[0:4]),int(date[4:6]),int(date[6:]),int(_time[0:2]),int(_time[2:4]),int(_time[4:]),-1,-1,-1))
+    except:import pdb;pdb.set_trace()
     os.utime(i, (media_time,media_time))

6b6bd48176bd42a50a1f8b26fb048ca0f190bd7a
Broshward <HarmoniesWorlds@gmail.com>
2024-08-31 00:21:03 +0300

    Show video engine will be here. At now it simple mplayer frontend

---
+++ show.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python2
+
+import sys,os
+filenames=sys.argv[1:]
+for i in filenames:
+    print i,' ',
+print
+filename = filenames[0]
+filename = os.path.realpath(filename)
+
+from join_video import *
+
+if is_image(filename):
+    cmd='feh -F %s' %(filename)
+elif is_video(filename):
+    cmd='mplayer -vf rotate=1,rotate=1 %s' %(filename)
+    cmd='mplayer %s' %(filename)
+    cmd='ffplay -fs %s' %(filename)
+    
+print cmd
+window=os.popen(cmd,'w')
+

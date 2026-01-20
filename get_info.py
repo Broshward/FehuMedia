9ee4e762a686ebba370c838ee9925c7ddcc57537
Broshward <harmoniesworlds@gmail.com>
2026-01-20 16:48:13 +0300

    python3 migration

--- get_info.py
+++ get_info.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python2
+#!/usr/bin/python
 
 # $1 - file name
 
@@ -15,52 +15,51 @@ filename = os.path.realpath(filename)
 
 data=os.popen('/usr/bin/vendor_perl/exiftool -g2 "%s" ' %(filename)).read()
 
-print 'Media:'
+print ('Media:')
 param='File Name'
-print data[data.find(param):].split('\n',1)[0]
+print(data[data.find(param):].split('\n',1)[0]) 
 param='File Size'
-print data[data.find(param):].split('\n',1)[0]
+print(data[data.find(param):].split('\n',1)[0]) 
 param='Directory'
-print data[data.find(param):].split('\n',1)[0]
+print(data[data.find(param):].split('\n',1)[0]) 
 param='Date/Time Original'
-print data[data.find(param):].split('\n',1)[0]
+print(data[data.find(param):].split('\n',1)[0]) 
 param='Exposure Compensation'
-print data[data.find(param):].split('\n',1)[0]
+print(data[data.find(param):].split('\n',1)[0]) 
 param='Image Size'
-print data[data.find(param):].split('\n',1)[0]
+print(data[data.find(param):].split('\n',1)[0]) 
 param='White Balance'
-print data[data.find(param):].split('\n',1)[0]
+print(data[data.find(param):].split('\n',1)[0]) 
 param='Shutter Speed'
-print data[data.find(param):].split('\n',1)[0]
+print(data[data.find(param):].split('\n',1)[0]) 
 param='Aperture'
-print data[data.find(param):].split('\n',1)[0]
+print(data[data.find(param):].split('\n',1)[0]) 
 param='Field Of View'
-print data[data.find(param):].split('\n',1)[0]
+print(data[data.find(param):].split('\n',1)[0]) 
 param='Focal Length'
-print data[data.find(param):].split('\n',1)[0]
+print(data[data.find(param):].split('\n',1)[0]) 
 param='Hyperfocal Distance'
-print data[data.find(param):].split('\n',1)[0]
+print(data[data.find(param):].split('\n',1)[0]) 
 
-print 'Video:'
+print('Video:') 
 param='Media Duration'
-print data[data.find(param):].split('\n',1)[0]
+print(data[data.find(param):].split('\n',1)[0]) 
 param='Video Frame Rate'
-print data[data.find(param):].split('\n',1)[0]
+print(data[data.find(param):].split('\n',1)[0]) 
 
-print 'Audio:'
+print('Audio:') 
 param='Hyperfocal Distance'
-print data[data.find(param):].split('\n',1)[0]
+print(data[data.find(param):].split('\n',1)[0]) 
 
-print 'Comment:'
+print('Comment:') 
 comment=os.popen("get_comment %s" % (filename)).read()
-print comment
-print
+print(comment,'\n') 
 
 if debug:
-    print 'All exif data:'
-    print data
-    print 'ffmpeg info:'
+    print('All exif data:') 
+    print(data) 
+    print('ffmpeg info:') 
     info=os.popen("ffmpeg -i %s 2>&1 "%(filename)).read()
-    print info[info.find('Input #'):]
+    print(info[info.find('Input #'):]) 
 
 
